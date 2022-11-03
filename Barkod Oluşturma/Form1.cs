49065364a4b21e3eef691524cf9e9b37b0b70015
yarenkaratas <yarenkrts98@gmail.com>
2022-11-03 15:49:58 +0300

    QRCode_olusturma_Barkod_okuma

---
+++ Barkod Oluşturma/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barkod_Oluşturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.Code128BarcodeDraw brc = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            pictureBox1.Image = brc.Draw(textBox1.Text, 35);  //barkod büyüklüğü 35 px 
+        }
+    }
+}
